ab43a7c8e9d44fd83c3d9b0dc308df4679bb23db
Joao Eduardo Mendes Chaicoski <joao.chaicoski@ambevtech.com.br>
2023-02-09 00:36:49 -0300

    Material installed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import './App.css';
+import React from 'react';
+import {Grid, Box} from "@mui/material";
+import {styled} from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({theme}) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Let's code 
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <body className="App-body">
+            <Box sx={
+                    {flexGrow: 1}
+                }>
+                    <Grid container
+                        spacing={2}>
+                        <Grid>
+                            <Item>Teste A</Item>
+                        </Grid>
+                        <Grid>
+                            <Item>Teste B</Item>
+                        </Grid>
+                        <Grid>
+                            <Item>Teste C</Item>
+                        </Grid>
+                        <Grid>
+                            <Item>Teste D</Item>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </body>
+        </div>
+    );
 }
 
 export default App;
